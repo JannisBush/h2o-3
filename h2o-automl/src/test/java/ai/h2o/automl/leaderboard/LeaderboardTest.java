5b6ee18d3aca128252c3a34c18553429837d5ed0
michalkurka <michalk@h2o.ai>
2021-07-22 20:13:38 -0400

    also fix leaderboard test

--- h2o-automl/src/test/java/ai/h2o/automl/leaderboard/LeaderboardTest.java
+++ h2o-automl/src/test/java/ai/h2o/automl/leaderboard/LeaderboardTest.java
@@ -79,7 +79,7 @@ public class LeaderboardTest extends water.TestUtil {
       lb = Leaderboard.getOrMake("dummy_rank_tsv", eventLog, null, "mae");
       lb.addModel(model._key);
       Log.info(lb.rankTsv());
-      assertEquals("Error\n[0.3448260574357465, 0.19959320678410908, 0.44675855535636816, 0.19959320678410908, 0.31468498072970547]\n", lb.rankTsv());
+      assertEquals("Error\n[0.3457753935260178, 0.1990694699054436, 0.4461720182905284, 0.1990694699054436, 0.31391918599301116]\n", lb.rankTsv());
     } finally {
       if (lb != null){
         lb.remove();
